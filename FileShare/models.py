7d1183d3e760532f7c6aef2fd4954398a86d3b02
Saquib <sakibsadman678@gmail.com>
2024-04-24 11:13:58 +0600

    File-Sharing-App

---
+++ FileShare/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    # Your model fields and methods
+    class Meta:
+        app_label = 'FileShare'
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    pwd = models.CharField(max_length=100)
+    gender = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class File_Upload(models.Model):
+    user = models.ForeignKey(User, models.CASCADE)
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    file_field = models.FileField(upload_to="")
+
+    def __str__(self):
+        return self.title
